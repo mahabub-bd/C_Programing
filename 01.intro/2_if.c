1f94f809acdb680d8937336741b7fe778364c1a4
Mahabub Hossain <64084426+mahabub-hossain-bd@users.noreply.github.com>
2021-01-18 21:12:07 +0600

    IF Condition Completed

---
+++ 01.intro/2_if.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    if (1)
+    {
+       printf("I Will executive\n");
+       int a = 35;
+       int b = 45;
+       if (a > b)
+       {
+          printf("A is Bigger\n");
+       }
+       
+    }
+    
+
+    return 0;
+}
