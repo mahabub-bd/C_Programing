e9fb17f312d159c7bf7a9182e6ab07740f6dc6e4
Mahabub Hossain <64084426+mahabub-hossain-bd@users.noreply.github.com>
2021-01-19 10:57:47 +0600

    Switch statement completed

---
+++ 01.intro/5_switch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(void)
+{
+    int day_number;
+    printf("Enter Day Number\n");
+    scanf("%d", &day_number);
+    switch (day_number)
+    {
+    case 0:
+        printf("Saturday\n");
+        break;
+    case 1:
+        printf("Sunday\n");
+        break;
+    case 2:
+        printf("Monday\n");
+        break;
+    case 3:
+        printf("Tuesday\n");
+        break;
+    case 4:
+        printf("Wednesday\n");
+        break;
+    case 5:
+        printf("Thusday\n");
+        break;
+    case 6:
+        printf("Friday\n");
+        break;
+
+    default:
+    printf("Wrong day\n");
+        break;
+    }
+    return 0;
+}
